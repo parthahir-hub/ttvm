848b0dcfc3d6f43b974f20ad32efef2e2f0e7d0d
parthahir-hub <forcoding63@gmail.com>
2026-06-03 18:49:07 +0530

    Add files via upload

---
+++ p12.py
@@ -0,0 +1,8 @@
+import streamlit as st
+st.header("my playlist")
+st.audio("https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3")
+st.audio("https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3")
+st.audio("https://www.soundhelix.com/examples/mp3/SoundHelix-Song-3.mp3")
+st.audio("https://www.soundhelix.com/examples/mp3/SoundHelix-Song-4.mp3")
+st.audio("https://www.soundhelix.com/examples/mp3/SoundHelix-Song-5.mp3")
+st.audio("https://www.soundhelix.com/examples/mp3/SoundHelix-Song-6.mp3")
