731655988a5585b378754d0cb177ab65c8d79a8b
parthahir-hub <forcoding63@gmail.com>
2026-06-05 17:45:37 +0530

    Add files via upload

---
+++ p14.py
@@ -0,0 +1,7 @@
+import streamlit as st
+st.title("rcb is the winner")
+st.image("https://cricketinshight.in/wp-content/uploads/2026/06/RCB-WON-CHAMPION.jpg")
+st.title("srh is the winner")
+st.image("https://images.indianexpress.com/2016/05/yuvi-m2.jpg?w=1200")
+
+
