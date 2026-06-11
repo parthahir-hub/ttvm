b2c87c5f6fcee17c5ca450593a8872c69f39ed30
parthahir-hub <forcoding63@gmail.com>
2026-06-11 18:05:31 +0530

    Add files via upload

---
+++ p21.py
@@ -0,0 +1,19 @@
+import streamlit as st
+num=st.number_input("enter number")
+if num==1:
+    st.write("Sunday")
+if num==2:
+    st.write("Monday")
+if num==3:
+    st.write("Tuesday")
+if num==4:
+    st.write("Wednesday")
+if num==5:
+    st.write("Thursday")
+if num==6:
+    st.write("Friday")
+if num==7:
+    st.write("Saturday")
+if num==8:
+    st.write("Holiday")
+                                    
